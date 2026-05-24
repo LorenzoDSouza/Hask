fba9e414a07d8d4266d3ffa804e0de4d88bd887e
LorenzoDSouza <lkirstdesouza@gmail.com>
2026-05-24 15:50:53 -0300

    Feat: add the task router base file and the auth router base file.

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 #no terminal pra rodar a api: uvicorn main:app --reload
+
+from auth_routes import auth_router
+from task_routes import task_router
+
+app.include_router(auth_router)
+app.include_router(task_router)
