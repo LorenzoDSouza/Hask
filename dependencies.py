0f7780d8e77c1d7c856e12cdcde49ac306003671
LorenzoDSouza <lkirstdesouza@gmail.com>
2026-05-28 21:42:44 -0300

    Fix: add the close() instruction using try catch to always close the db session.

--- dependencies.py
+++ dependencies.py
@@ -2,5 +2,9 @@ from models import db
 from sqlalchemy.orm import sessionmaker
 
 def get_session():
-    Session = sessionmaker(bind=db)
-    return Session()
+    try:
+        Session = sessionmaker(bind=db)
+        session = Session()
+        yield session
+    finally:
+        session.close()
