c3ede9858ca8a9bbf1745b352bce982872f73e3a
LorenzoDSouza <lkirstdesouza@gmail.com>
2026-05-28 21:27:23 -0300

    Feat: create the first method to post a user (with a lot of smells that will be fixed).

--- auth_routes.py
+++ auth_routes.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter
+from models import User, db
+from sqlalchemy.orm import sessionmaker
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -8,3 +10,17 @@ async def authenticate():
     This is the standardt authentication route for the API
     """
     return {"message": "Authentication in process", "authenticated": False}
+
+@auth_router.post("/create_user")
+async def create_user(email: str, password: str, name: str):
+    Session = sessionmaker(bind=db)
+    session = Session()
+    user = session.query(User).filter(User.email==email).first()
+
+    if user:
+        return {"message": "Email alredy been used by another user!"}
+    else:
+        new_user = User(name, email, password)
+        session.add(new_user)
+        session.commit()
+        return {"message": "User created succesfully!"}
