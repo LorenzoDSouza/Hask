6e717ac51fccf3c01dde08b4708bd922fca3cd16
LorenzoDSouza <lkirstdesouza@gmail.com>
2026-05-28 21:58:24 -0300

    Feat: add the bcrypt to secutiry in the users passwords.

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
 
-app = FastAPI()
-#no terminal pra rodar a api: uvicorn main:app --reload
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+app = FastAPI() #no terminal pra rodar a api: uvicorn main:app --reload
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 from auth_routes import auth_router
 from task_routes import task_router
