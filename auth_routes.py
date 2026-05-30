fc79bc0ab8dcc9cff53e26e98fd1a1a3ea42c83c
LorenzoDSouza <lkirstdesouza@gmail.com>
2026-05-29 22:37:15 -0300

    feat: authentication route created, but not finished. At this point its just a dummy function for login, need to confirm password related to email and finish the process of generation of the token.

--- auth_routes.py
+++ auth_routes.py
@@ -2,10 +2,14 @@ from fastapi import APIRouter, Depends, HTTPException
 from models import User
 from dependencies import get_session
 from main import bcrypt_context
-from schemas import UserRequest
+from schemas import UserRequest, LoginRequest
 from sqlalchemy.orm import Session
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
+
+def create_token(user_id):
+    token = f"sci1909{user_id}"
+    return token
 
 @auth_router.get("/")
 async def authenticate():
@@ -27,3 +31,13 @@ async def create_user(user_request: UserRequest, session: Session = Depends(get_
         session.commit()
         session.refresh(new_user)
         return {"message": "User {new_user.name} with email {new_user.email} created succesfully!"}
+    
+@auth_router.post("/login")
+async def login(login_request: LoginRequest, session: Session = Depends(get_session)):
+    user = session.querry(User).filter(User.email==login_request.email).first()
+
+    if not user:
+        raise HTTPException(status_code=400, detail="User not found with email {login_request.email}!")
+    else:
+        access_token = create_token(user.id)
+        return {"access_token": access_token,  "token_type" : "Bearer"}
--- schemas.py
+++ schemas.py
@@ -18,3 +18,10 @@ class TaskRequest(BaseModel):
     
     class Config:
         from_attributes = True
+
+class LoginRequest(BaseModel):
+    email: str
+    password: str
+
+    class Config:
+        from_attributes = True
