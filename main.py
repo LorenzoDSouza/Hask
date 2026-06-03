4212a3ecd54fac118139d490059e641f1d942e01
LorenzoDSouza <lkirstdesouza@gmail.com>
2026-06-03 19:49:03 -0300

    refactor: split the auth router that used to include both user and auth routes, but now each one has it's router file.

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ app = FastAPI() #no terminal pra rodar a api: uvicorn main:app --reload
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-from auth_routes import auth_router
-from task_routes import task_router
+from repository.auth_routes import auth_router
+from repository.task_routes import task_router
+from repository.user_routes import user_router
 
 app.include_router(auth_router)
 app.include_router(task_router)
+app.include_router(user_router)
--- repository/user_routes.py
+++ repository/user_routes.py
@@ -2,42 +2,14 @@ from fastapi import APIRouter, Depends, HTTPException
 from models import User
 from dependencies import get_session
 from main import bcrypt_context, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY
-from schemas import UserRequest, LoginRequest
+from schemas import UserRequest
 from sqlalchemy.orm import Session
 from jose import jwt, JWTError
 from datetime import datetime, timedelta, timezone
 
-auth_router = APIRouter(prefix="/auth", tags=["auth"])
+user_router = APIRouter(prefix="/users", tags=["users"])
 
-def create_token(user_id):
-    expire_date = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    dic_info = {"sub": user_id, "exp_date": expire_date}
-
-    encoded_jwt = jwt.encode(dic_info, SECRET_KEY, ALGORITHM)
-
-    return encoded_jwt
-
-def auth_user(email: str, password: str, session: Session = Depends(get_session)):
-    user = session.query(User).filter(User.email==email).first()
-
-    if not user:
-        return False
-    
-    if not bcrypt_context.verify(password, user.password):
-        return False
-
-    return user
-
-@auth_router.get("/")
-async def home():
-    """
-    This is the standardt authentication route for the API
-    """
-    return {"message": "You accessed the standardt auth route", "authenticated": False}
-
-
-
-@auth_router.post("/users")
+@user_router.post("/users")
 async def create_user(user_request: UserRequest, session: Session = Depends(get_session)):
     user = session.query(User).filter(User.email==user_request.email).first()
 
@@ -53,8 +25,7 @@ async def create_user(user_request: UserRequest, session: Session = Depends(get_
     return {"message": "User {new_user.name} with email {new_user.email} created successfully!"}
 
 
-
-@auth_router.get("/users/{user_id}")
+@user_router.get("/users/{user_id}")
 async def get_user_by_id(user_id: int, session: Session = Depends(get_session)):
     user = session.query(User).filter(User.id==user_id).first()
 
@@ -65,7 +36,7 @@ async def get_user_by_id(user_id: int, session: Session = Depends(get_session)):
 
 
 
-@auth_router.delete("/users/{user_id}")
+@user_router.delete("/users/{user_id}")
 async def delete_user_by_id(user_id: int, session: Session = Depends(get_session)):
     user = session.query(User).filter(User.id==user_id).first()
 
@@ -79,7 +50,7 @@ async def delete_user_by_id(user_id: int, session: Session = Depends(get_session
 
 
 
-@auth_router.put("/users/{user_id}")
+@user_router.put("/users/{user_id}")
 async def update_user_by_id(user_id: int, user_request: UserRequest, session: Session = Depends(get_session)):
     user = session.query(User).filter(User.id==user_id).first()
 
@@ -97,13 +68,3 @@ async def update_user_by_id(user_id: int, user_request: UserRequest, session: Se
     session.refresh(user)
 
     return user
-
-@auth_router.post("/login")
-async def login(login_request: LoginRequest, session: Session = Depends(get_session)):
-    user = auth_user(login_request.email, login_request.password, session)
-
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found with email {login_request.email}!")
-    else:
-        access_token = create_token(user.id)
-        return {"access_token": access_token,  "token_type" : "Bearer"}
