6777a966f9ba4dd7b4e8fc669c0a4f8d2194cc2f
LorenzoDSouza <lkirstdesouza@gmail.com>
2026-06-04 17:35:42 -0300

    feat: create the update function in the task_routes.

--- repository/task_routes.py
+++ repository/task_routes.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
-from models import Task
+from models import Task, User
 from dependencies import get_session
 from sqlalchemy.orm import Session
 from schemas import TaskRequest
@@ -21,10 +21,15 @@ async def create_task(task_request: TaskRequest, session: Session = Depends(get_
     session.add(new_task)
     session.commit()
     session.refresh(new_task)
-    return {"Task '{new_task.title}' created succesfully! New task id: {new_task.id}"}
+    return {f"Task '{new_task.title}' created succesfully! New task id: {new_task.id}"}
 
 @task_router.get("/tasks/{user_id}/{task_id}")
-async def get_user_by_id(user_id: int, task_id: int, session: Session = Depends(get_session)):
+async def get_task_by_id(user_id: int, task_id: int, session: Session = Depends(get_session)):
+    user = session.query(User).filter(User.id==user_id).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail=f"User not found with id {user_id}!")
+    
     task = session.query(Task).filter(Task.user_id==user_id & Task.id==task_id).first()
 
     if not task:
@@ -34,6 +39,11 @@ async def get_user_by_id(user_id: int, task_id: int, session: Session = Depends(
 
 @task_router.delete("/{user_id}/{task_id}")
 async def delete_task_by_id(user_id: int, task_id: int,  session: Session = Depends(get_session)):
+    user = session.query(User).filter(User.id==user_id).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail=f"User not found with id {user_id}!")
+
     task = session.query(Task).filter(Task.user_id==user_id & Task.id==task_id).first()
 
     if not task:
@@ -43,3 +53,30 @@ async def delete_task_by_id(user_id: int, task_id: int,  session: Session = Depe
     session.commit()
 
     return {"message": "User deleted successfully!"}
+
+@task_router.put("/{user_id}/{task_id}")
+async def update_task_by_id(task_request: TaskRequest, user_id: int, task_id: int, session: Session = Depends(get_session)):
+    user = session.query(User).filter(User.id==user_id).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail=f"User not found with id {user_id}!")
+    
+    task = session.query(Task).filter(Task.user_id==user_id & Task.task_id== task_id).first()
+
+    if not task:
+        raise HTTPException(status_code=404, detail=f"Task not found with id {task_id}!")
+    
+    new_user_assigned = session.query(User).filter(User.id==task_request.user_id).first()
+    
+    if not new_user_assigned:
+        raise HTTPException(status_code=404, detail=f"The new user you are trying to assign to the task does not exist: {user_id}!")
+
+    task.title = task_request.title
+    task.user_id = task_request.user_id
+    task.category = task_request.category
+    task.status = task_request.status
+
+    session.commit()
+    session.refresh(task)
+
+    return task
